c8f25ef79983e35b188f9df57d2058b851189779
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-27 03:22:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(params[:username])
-    session[:id]
+    session[:id] = @user.id
     redirect to '/account'
   end
 
