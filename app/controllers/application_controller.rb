d72ece59c6f4ccd8aa75bbbc417442fcc664840c
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-27 03:39:23 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
     if @user
       session[:user_id] = @user.id
       redirect to '/account'
-    else
+    else !@user
       erb :error
     end
   end
