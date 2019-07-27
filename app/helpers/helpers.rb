659d0189deba9f36d6df841b7987ece252c88391
Indre Bakeer <indre.bakeer@gmail.com>
2019-07-27 04:06:25 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -6,7 +6,7 @@ class Helpers
     user
   end
 
-#should return true if the user_id is in the session hash and false if not.
+# should return true if the user_id is in the session hash and false if not.
   def self.is_logged_in?(session)
     User.find(session[:user_id]) ? true : false
   end
